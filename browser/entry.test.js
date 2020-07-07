89b5808c9e29ee2bea6f29b61330d31b836b639b
Kojiro Ozaki <koujirou0111@gmail.com>
2020-07-07 18:07:28 +0900

    Add test

---
+++ browser/entry.test.js
@@ -0,0 +1,11 @@
+/* global test, expect */
+require('./entry');
+
+test('window に romajiConv 関数がある', () => {
+  expect(typeof window['romajiConv']).toBe('function');
+  expect(typeof window.romajiConv).toBe('function');
+});
+
+test('window で動作する', () => {
+  expect(window.romajiConv('hoge')).toBe('ほげ');
+});
