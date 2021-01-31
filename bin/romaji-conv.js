881a7c1d548b59250376a7881bd86deb712f928b
Markus Dam <markus.dam123@gmail.com>
2021-01-31 20:45:38 +0100

    Add option to force ん to be converted to two n's

--- bin/romaji-conv.js
+++ bin/romaji-conv.js
@@ -1,27 +1,24 @@
 #!/usr/bin/env node
 
-const { Command } = require('commander');
-const package = require('../package');
-const romajiConv = require('../lib/index');
+const { Command } = require("commander");
+const package = require("../package");
+const romajiConv = require("../lib/index");
 
 const program = new Command();
 
-
 program
-  .option('--katakana')
-  .option('--romaji')
+  .option("--katakana")
+  .option("--romaji")
+  .option("--doubleN")
   .action((someString, options) => {
     const instance = romajiConv(someString);
     if (options.katakana) {
       console.log(instance.toKatakana());
     } else if (options.romaji) {
-      console.log(instance.toRomaji());
+      console.log(instance.toRomaji(!!options.doubleN));
     } else {
       console.log(instance.toHiragana());
     }
   });
 
-program
-  .version(package.version)
-  .arguments('<someString>')
-  .parse(process.argv);
+program.version(package.version).arguments("<someString>").parse(process.argv);
