752d76964409ea85316c8166b5946d6689ae2151
Kojiro Ozaki <koujirou0111@gmail.com>
2023-05-30 20:19:39 +0900

    Add Prettier setting

--- bin/romaji-conv.js
+++ bin/romaji-conv.js
@@ -6,15 +6,9 @@ const romajiConv = require('../lib/index');
 
 const program = new Command();
 
+program.option('--katakana').action((someString, options) => {
+  const instance = romajiConv(someString);
+  console.log(options.katakana ? instance.toKatakana() : instance.toHiragana());
+});
 
-program
-  .option('--katakana')
-  .action((someString, options) => {
-    const instance = romajiConv(someString);
-    console.log(options.katakana ? instance.toKatakana() : instance.toHiragana());
-  });
-
-program
-  .version(package.version)
-  .arguments('<someString>')
-  .parse(process.argv);
+program.version(package.version).arguments('<someString>').parse(process.argv);
